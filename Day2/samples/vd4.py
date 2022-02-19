f1c75a25b7165166ce3d27708a23baf9552e2168
Tung Duong <duongthanhtungvn01@gmail.com>
2022-02-19 09:17:42 +0700

    Update

--- Day2/samples/vd4.py
+++ Day2/samples/vd4.py
@@ -3,7 +3,7 @@ Chỉ số BMI được dùng để đánh giá tình trạng gầy, béo của 
     bmi = w/(h*h)
 
 Trong đó h là chiều cao tính theo đơn vị mét, w là cân nặng tính theo đơn vị kg. Dựa vào chỉ số bmi, một người được đánh giá như sau:
-    ○ Gầy: nếubmi<16
+    ○ Gầy: nếu bmi<16
     ○ Hơi gầy: nếu bmi 16 ~ 18.5
     ○ Bình thường: nếu bmi 18.5~25
     ○ Hơibéo: nếubmi25~30
