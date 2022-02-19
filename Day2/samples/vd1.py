14c9f0b9de0b632a6221d68660d6b8c58486f94e
Tung Duong <duongthanhtungvn01@gmail.com>
2022-02-19 09:11:40 +0700

    Update

---
+++ Day2/samples/vd1.py
@@ -0,0 +1,14 @@
+'''
+Nhập vào 2 số từ bàn phím, in ra số lớn hơn
+'''
+
+a = input('Số thứ nhất:')
+a = float(a)
+
+b = input('Số thứ hai:')
+b = float(b)
+
+if a > b:
+    print('Số lớn hơn:', a)
+else:
+    print('Số lớn hơn:', b)
---
+++ Day2/samples/vd4.py
@@ -0,0 +1,35 @@
+'''
+Chỉ số BMI được dùng để đánh giá tình trạng gầy, béo của một người. Chỉ số này được tính theo công thức:
+    bmi = w/(h*h)
+
+Trong đó h là chiều cao tính theo đơn vị mét, w là cân nặng tính theo đơn vị kg. Dựa vào chỉ số bmi, một người được đánh giá như sau:
+    ○ Gầy: nếubmi<16
+    ○ Hơi gầy: nếu bmi 16 ~ 18.5
+    ○ Bình thường: nếu bmi 18.5~25
+    ○ Hơibéo: nếubmi25~30
+    ○ Béo: nếu bmi > 30
+Nhập vào từ bàn phím chiều cao & cân nặng của một người, in ra tình trạng gầy/béo của người đó
+'''
+
+height = input('Chiều cao (mét) : ')
+height = float(height)
+
+mass = input('Cân nặng (kg) : ')
+mass = float(mass)
+
+bmi = mass / (height * height)
+
+if bmi < 16:
+    print('Thân hình gầy')
+
+elif bmi < 18.5:
+    print('Thân hình hơi gầy')
+
+elif bmi < 25:
+    print('Thân hình bình thường')
+
+elif bmi < 30:
+    print('Thân hình hơi béo')
+
+else:
+    print('Thân hình béo')
