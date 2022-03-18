a6915ff68483f193aff7602102695da35e241da2
Tung Duong <duongthanhtungvn01@gmail.com>
2022-03-18 21:08:09 +0700

    Update

--- Day-8/vd3.py
+++ Day-8/vd3.py
@@ -7,20 +7,25 @@ class Patient:
         self.birth_date = birth_date
 
     def to_json(self):
-        return json.dumps({
-            'name': ...,
-            'mrn': ...,
-            'birth_date': ...
-        })
-
+        data = {
+            'name': self.name,
+            'mrn': self.mrn,
+            'birth_date': self.birth_date
+        }
+        return json.dumps(data)
+    # -------------------------------------------------------
     def from_json(text):
-        data = json.loads(...)
-        return Patient(...)
+        data = json.loads(text)
+        name = data['name']
+        mrn = data['mrn']
+        birth_date = data['birth_date']
+        return Patient(name, mrn, birth_date)
 
 patient = Patient('Nguyen Van A', 'HN023212', '2000-01-01')
 text = patient.to_json()
 print(text)
 
-# ---------------------------------------------------------
+print('---------------------------------------------------------')
 
 patient = Patient.from_json(text)
+print(patient.name, patient.mrn, patient.birth_date)
