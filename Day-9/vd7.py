01ff1f51a9699893ff3fcc4694fc7658f07ace4c
Tung Duong <duongthanhtungvn01@gmail.com>
2022-03-23 21:35:03 +0700

    Update

---
+++ Day-9/vd7.py
@@ -0,0 +1,42 @@
+import unittest
+
+#Viết hàm validate mật khẩu, yêu cầu:
+# + Độ dài tối thiểu là 6
+# + Chứa ít nhất một chữ cái
+# + Chứa ít nhất một chữ số
+
+def check_contain_char(st, c_min, c_max):
+    for c in st:
+        if c_min <= c <= c_max:
+            return True
+
+    return False
+
+def validate_password(password):
+    if len(password) < 6:
+        return False
+
+    if not check_contain_char(password, '0', '9'):
+        return False
+
+    return (
+        check_contain_char(password, 'a', 'z') or
+        check_contain_char(password, 'A', 'Z')
+    )
+
+class TestValidatePassword(unittest.TestCase):
+    def test_password_too_short(self):
+        self.assertFalse(validate_password('abc12'))
+
+    def test_password_no_alpha(self):
+        self.assertFalse(validate_password('123456'))
+
+    def test_password_no_digit(self):
+        self.assertFalse(validate_password('abcdef'))
+
+    def test_password_ok(self):
+        self.assertTrue(validate_password('abdef1'))
+        self.assertTrue(validate_password('12345a'))
+
+if __name__ == '__main__':
+    unittest.main()
