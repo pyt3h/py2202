b930aa540a6e0b8fb458dc4a74f4d1b2f0eb89c2
Tung Duong <duongthanhtungvn01@gmail.com>
2022-04-13 20:44:05 +0700

    Update

--- Day-15/svelte-app/src/App.svelte
+++ Day-15/svelte-app/src/App.svelte
@@ -12,38 +12,30 @@
       </thead>
       <tbody id="table_body">
         {#each data as row}
-          <tr>...</tr>
+          <tr>
+            <td>{row?.time ?? '-'}</td>
+            <td>{row?.main?.temp ?? '-'}</td>
+            <td>{row?.main?.humidity ?? '-'}</td>
+            <td>{row?.main?.pressure ?? '-'}</td>
+          </tr>
         {/each}
       </tbody>
   </table>
 </div>
 <script>
   let url = 'http://api.openweathermap.org/data/2.5/forecast?id=1581129&units=metric&appid=d6477696b63c2e661af64eead58c11d9&cnt=8';
-  let data;
+  let data = [];
   /*   
   fetch(url)
       .then(resp => resp.json())
       .then(data => console.log(data));
   */
 
-  function renderRow(row){
-      return (`
-          <tr>	
-              <td>${row?.time ?? '-'}</td>
-              <td>${row?.main?.temp ?? '-'}</td>
-              <td>${row?.main?.humidity ?? '-'}</td>
-              <td>${row?.main?.pressure ?? '-'}</td>
-          </tr>`
-      );
-  };
-
   async function init() {
       let resp = await fetch(url);
       data = await resp.json();
       data = data.list.map(item => ({main: item.main, time: item.dt_txt}));
       console.log(data);
-      //let html = data.map(renderRow).join('');
-      //document.getElementById('table_body').innerHTML = html;
   }
   
   init(); //main
