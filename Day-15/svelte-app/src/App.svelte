a7b91947d293d60f69cc584181567e1cefefd9fa
Tung Duong <duongthanhtungvn01@gmail.com>
2022-04-13 20:35:42 +0700

    Update

--- Day-15/svelte-app/src/App.svelte
+++ Day-15/svelte-app/src/App.svelte
@@ -1,32 +1,46 @@
-<div class="container mt-3">
-    <h3>Thời tiết 24h tới</h3>
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>Thời gian</th>
-                <th>Nhiệt độ °C</th>
-                <th>Độ ẩm (%)</th>
-                <th>Áp suất khí quyển (atm)</th>
-            </tr>
-        </thead>
-        <tbody id="table_body">
-          {#each data as row}
-            <tr>	
-              <td>{row.time}</td>
-              <td>{row.temp}</td>
-              <td>{row.humdity}</td>
-              <td>{row.pressure}</td>
-            </tr>
-          {/each}
-        </tbody>
-    </table>
-</div>
 
+<div class="container mt-3">
+  <h3>Thời tiết 24h tới</h3>
+  <table class="table table-bordered">
+      <thead>
+          <tr>
+              <th>Thời gian</th>
+              <th>Nhiệt độ °C</th>
+              <th>Độ ẩm (%)</th>
+              <th>Áp suất khí quyển (atm)</th>
+          </tr>
+      </thead>
+      <tbody id="table_body"></tbody>
+  </table>
+</div>
 <script>
-    let data = [
-        {time: '2022-03-25 15:00:00', temp: 24.5, humdity: 88, pressure: 1.008},
-        {time: '2022-03-25 18:00:00', temp: 24.17, humdity: 91, pressure: 1.007},
-        {time: '2022-03-25 21:00:00', temp: 23.12, humdity: 92, pressure: 1.009},
-        {time: '2022-03-26 00:00:00', temp: 22.26, humdity: 90, pressure: 1.012},
-    ];
+  let url = 'http://api.openweathermap.org/data/2.5/forecast?id=1581129&units=metric&appid=d6477696b63c2e661af64eead58c11d9&cnt=8';
+  let data;
+  /*   
+  fetch(url)
+      .then(resp => resp.json())
+      .then(data => console.log(data));
+  */
+
+  function renderRow(row){
+      return (`
+          <tr>	
+              <td>${row?.time ?? '-'}</td>
+              <td>${row?.main?.temp ?? '-'}</td>
+              <td>${row?.main?.humidity ?? '-'}</td>
+              <td>${row?.main?.pressure ?? '-'}</td>
+          </tr>`
+      );
+  };
+
+  async function init() {
+      let resp = await fetch(url);
+      data = await resp.json();
+      data = data.list.map(item => ({main: item.main, time: item.dt_txt}));
+      console.log(data);
+      //let html = data.map(renderRow).join('');
+      //document.getElementById('table_body').innerHTML = html;
+  }
+  
+  init(); //main
 </script>
