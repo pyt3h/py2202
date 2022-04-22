ecf0ae2751e39d47835d4be94e0f5ab9fae57834
Tung Duong <duongthanhtungvn01@gmail.com>
2022-04-22 21:37:56 +0700

    Update

--- Day-18/prj_day18/app/urls.py
+++ Day-18/prj_day18/app/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
    path('', index),
    path('hello', hello),
+   path('add-2-number', add_2_number),
+   path('get-weather-data', get_weather_data),
 ]
 
--- Day-18/prj_day18/app/views.py
+++ Day-18/prj_day18/app/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
@@ -6,4 +7,26 @@ def index(request):
     return render(request, 'index.html')
 
 def hello(request):
-    return HttpResponse("Hello")
+    data = request.GET
+    username = data.get('username', '')
+    return HttpResponse(f"<b>Hello {username}</b>")
+
+def add_2_number(request):
+    data = request.GET
+    print('data=', data)
+    a = float(data.get('a', 0))
+    b = float(data.get('b', 0))
+    c = a + b
+    return HttpResponse(f'Tổng 2 số : {c}')
+
+data = {
+  'Hanoi': {'temp': 19, 'humidity': 90},
+  'HCMCity': {'temp': 32, 'humidity': 80},
+}
+
+def get_weather_data(request):
+    location = request.GET.get('location')
+    result = data.get(location, {'error': 'Unknown location'})
+    
+    return HttpResponse(json.dumps(result), 
+            content_type='application/json')
