aa349cb97e882d1a7ab16522f47fae763fdfd505
Tung Duong <duongthanhtungvn01@gmail.com>
2022-04-27 20:26:18 +0700

    update

--- Day-19/project_day19/app/models.py
+++ Day-19/project_day19/app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Book(models.Model):
+    code = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+    published_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+# --------------------------- Shop -----------------------------------
+class ProductCategory(models.Model):
+    code = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey('ProductCategory', on_delete=models.PROTECT, null=True, blank=True)
