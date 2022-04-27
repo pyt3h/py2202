35dc7f6c4cdc352714d649b2153e2d192f06f67c
Tung Duong <duongthanhtungvn01@gmail.com>
2022-04-27 21:47:47 +0700

    Update

--- Day-19/project_day19/app/views.py
+++ Day-19/project_day19/app/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+import json
+from django.shortcuts import render, HttpResponse
+from .models import *
 
-# Create your views here.
+def search_book(request):
+    params = request.GET
+    keyword = params.get('keyword', '')
+    book_list = Book.objects.filter(name__icontains=keyword)
+    print('book_list=', book_list)
+    items = []
+    for book in book_list:
+        items.append({'id': book.id, 'name': book.name})
+    result = json.dumps({'books': items})
+    return HttpResponse(result)
 
 def index(request):
     return render(request, 'index.html')
