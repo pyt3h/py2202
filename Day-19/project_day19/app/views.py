aec9a44b8f71fbcfcd7de6fc1157174dc8940e0a
Tung Duong <duongthanhtungvn01@gmail.com>
2022-04-27 22:03:40 +0700

    Update

--- Day-19/project_day19/app/views.py
+++ Day-19/project_day19/app/views.py
@@ -5,7 +5,13 @@ from .models import *
 def search_book(request):
     params = request.GET
     keyword = params.get('keyword', '')
-    book_list = Book.objects.filter(name__icontains=keyword)
+    from_year = params.get('from_year', 1900)
+    to_year = params.get('to_year', 2100)
+    book_list = Book.objects.filter(
+        name__icontains=keyword,
+        published_year__lt=to_year,
+        published_year__gt=from_year
+    )
     print('book_list=', book_list)
     items = []
     for book in book_list:
