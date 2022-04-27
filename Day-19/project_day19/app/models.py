6cf372a781dc52a7a390afc74810692861bc2a8e
Tung Duong <duongthanhtungvn01@gmail.com>
2022-04-27 20:41:34 +0700

    Update

--- Day-19/project_day19/app/models.py
+++ Day-19/project_day19/app/models.py
@@ -21,10 +21,22 @@ class ProductCategory(models.Model):
     parent = models.ForeignKey('ProductCategory', on_delete=models.PROTECT, null=True, blank=True)
 
 class Product(models.Model):
-    ...
+    code = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    image_url = models.CharField(max_length=1024)
+    category = models.ForeignKey(ProductCategory, on_delete=models.PROTECT)
 
 class Customer(models.Model):
-    ...
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
 
 class SaleOrder(models.Model):
-    ...
+    qty = models.IntegerField()
+    price_unit = models.IntegerField()
+    total = models.IntegerField()
+    order_date = models.DateTimeField()
+    status = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
