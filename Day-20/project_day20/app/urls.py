006710bfb5c5d666686f8f5eb23c0f7240a324b7
Tung Duong <duongthanhtungvn01@gmail.com>
2022-05-06 20:09:36 +0700

    Update

--- Day-20/project_day20/app/urls.py
+++ Day-20/project_day20/app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
    path('search-book', search_book),
    path('borrow-book', borrow_book),
    path('test-post', test_post),
+   path('get-user-borrow-list', get_user_borrow_list),
 ]
 
