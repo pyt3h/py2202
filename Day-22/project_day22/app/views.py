06f9f9205407a9631123d786f4a154e58d1e8af3
Tung Duong <duongthanhtungvn01@gmail.com>
2022-05-11 20:42:20 +0700

    Update

--- Day-22/project_day22/app/views.py
+++ Day-22/project_day22/app/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import *
 
 @api_view(['GET', 'POST'])
 def hello(request):    
@@ -11,6 +12,7 @@ def hello(request):
 def create_customer(request):
    data = request.data
    print('data=', data)
-   # TODO: Validate & Save database
+   
+  
    return Response({"success": True})
 
