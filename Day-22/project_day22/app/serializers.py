18f48504080765b3ca76608df6700f3e9a3dd11f
Tung Duong <duongthanhtungvn01@gmail.com>
2022-05-11 21:41:57 +0700

    Update

--- Day-22/project_day22/app/serializers.py
+++ Day-22/project_day22/app/serializers.py
@@ -6,3 +6,13 @@ class CustomerSerializer(ModelSerializer):
     class Meta:
         model = Customer
         fields = '__all__'
+
+class ProductCategorySerializer(ModelSerializer):
+    class Meta:
+        model = ProductCategory
+        fields = '__all__'
+
+class ProductSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
