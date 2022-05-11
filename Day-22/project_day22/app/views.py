5e3127b63a7487fd83ea953a6f03e02cd222a419
Tung Duong <duongthanhtungvn01@gmail.com>
2022-05-11 20:45:48 +0700

    Update

--- Day-22/project_day22/app/views.py
+++ Day-22/project_day22/app/views.py
@@ -13,6 +13,11 @@ def create_customer(request):
    data = request.data
    print('data=', data)
    
+   serializer = CustomerSerializer(data=data)
+   if serializer.is_valid():
+      return Response(serializer.errors, status=400)
+   
+   serializer.save()
   
    return Response({"success": True})
 
