4e208d938d6ccde31e18457478520a861a1ec91f
Tung Duong <duongthanhtungvn01@gmail.com>
2022-05-13 19:39:38 +0700

    Update

--- Day-23/project_day23/app/views.py
+++ Day-23/project_day23/app/views.py
@@ -3,4 +3,4 @@ from rest_framework.response import Response
 
 @api_view(['GET', 'POST'])
 def hello(request):    
-   return Response({"message" : "Hello world!"})
+   return Response({"message" : "Hello world 123!"})
