bcb717fb746e9495613b93504df553bf81ba06af
Tung Duong <duongthanhtungvn01@gmail.com>
2022-05-18 20:16:44 +0700

    Update

--- Day-24/project_day24/app/urls.py
+++ Day-24/project_day24/app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('search-customer', search_customer),
     path('hello', hello)
 ]
--- Day-24/project_day24/app/views.py
+++ Day-24/project_day24/app/views.py
@@ -4,3 +4,19 @@ from rest_framework.response import Response
 @api_view(['GET', 'POST'])
 def hello(request):    
    return Response({"message" : "Hello world!"})
+
+from rest_framework.serializers import ModelSerializer
+from .models import *
+
+class CustomerSerializer(ModelSerializer):
+   class Meta:
+      model = Customer
+      fields = '__all__'
+
+@api_view(['GET'])
+def search_customer(request):
+   params = request.GET
+   keyword = params.get('keyword', '')
+   customer_list = Customer.objects.filter(phone__icontains=keyword)
+   result = CustomerSerializer(customer_list, many=True).data
+   return Response(result)
