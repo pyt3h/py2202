b8d4992d52c74fefe7ad49505913bcd2bb553531
Tung Duong <duongthanhtungvn01@gmail.com>
2022-05-18 20:34:50 +0700

    Update

--- Day-24/project_day24/app/urls.py
+++ Day-24/project_day24/app/urls.py
@@ -5,5 +5,7 @@ from .views import *
 urlpatterns = [
     path('search-customer', search_customer),
     path('get-customer-by-phone/<phone>', get_customer_by_phone),
+    path('search-product', search_product),
+    path('get-product-by-code/<code>', get_product_by_code),
     path('hello', hello)
 ]
